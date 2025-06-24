db3fc2d8a5805a81e07c04ed561721e1867ca478
Paweł Tomaszek <ptomaszek@users.noreply.github.com>
2025-06-24 21:57:28 +0200

    Merge listeners and extension

--- src/test/groovy/org/example/extensions/IBlockLoggingExtension.groovy
+++ src/test/groovy/org/example/extensions/IBlockLoggingExtension.groovy
@@ -1,12 +1,35 @@
 package org.example.extensions
 
+import org.slf4j.LoggerFactory
+import org.spockframework.runtime.AbstractRunListener
+import org.spockframework.runtime.extension.IBlockListener
 import org.spockframework.runtime.extension.IGlobalExtension
+import org.spockframework.runtime.model.BlockInfo
+import org.spockframework.runtime.model.FeatureInfo
 import org.spockframework.runtime.model.SpecInfo
+import spock.lang.Specification
 
 class IBlockLoggingExtension implements IGlobalExtension {
 
     @Override
     void visitSpec(SpecInfo spec) {
         spec.addListener(new BlockLoggingRunListener())
+    }
+
+    class BlockLoggingRunListener extends AbstractRunListener {
+
+        @Override
+        void beforeFeature(FeatureInfo feature) {
+            feature.addBlockListener(new BlockLoggingListener())
+        }
+    }
+
+    class BlockLoggingListener implements IBlockListener {
+
+        @Override
+        <S extends Specification> void blockEntered(S specificationInstance, BlockInfo blockInfo) {
+            def logger = LoggerFactory.getLogger(specificationInstance.class)
+            logger.info("{}", blockInfo.getTexts().join("; "))
+        }
     }
 }
